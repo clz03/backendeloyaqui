be731793020d2d511c5142376a828ca9c4f33cc7
DEV <dev@W80279SCDB7.local>
2019-10-20 22:34:08 -0300

    Adicionando cors Security

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
@@ -9,6 +10,7 @@ mongoose.connect(process.env.DATABASE_URI, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 server.use(express.json());
 server.use(routes);
 
