0bfc5f3002649c87b872155481091867c30ba4ee
DEV <dev@W80279SCDB7.local>
2019-11-03 21:55:42 -0300

    adicionado nodemailer

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../model/Usuario');
+const nodemailer = require('nodemailer');
 
 module.exports = {
 
@@ -28,7 +29,7 @@ module.exports = {
         return res.json(returnDel)
     },
 
-    store(req, res) {
+    async store(req, res) {
         const { email, pwd, validado, nome } = req.body;
 
         const returnPost = Usuario.create({
@@ -38,7 +39,35 @@ module.exports = {
             nome
         });
 
-        return res.json(returnPost);
-    },
+        console.log('Post feito', res.json(returnPost));
 
+        let transporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "cb617fac631548",
+              pass: "e6242a10de30be"
+            }
+        });
+
+        console.log('Transporter feito');
+
+        let info = await transporter.sendMail({
+            from: '"EloyAqui 👻" <noreply@eloyaqui.com.br>', // sender address
+            to: email, // list of receivers
+            subject: 'Confirme seu e-mail ✔', // Subject line
+            text: 'Termine seu cadastro efetuando a confirmação', // plain text body
+            html: '<b>do seu email</b>' // html body
+        });
+
+        console.log('email enviado feito');
+
+        console.log('Message sent: %s', info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    },
 };
