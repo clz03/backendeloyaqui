0eb9cca7ff7d7595b5f80c86d474e7c30e3be36a
DEV <dev@W80279SCDB7.local>
2019-11-22 15:24:43 -0300

    alteracao dias evento

--- src/controllers/EventoController.js
+++ src/controllers/EventoController.js
@@ -1,5 +1,8 @@
 const Evento = require('../model/Evento');
+const Usuario = require('../model/Usuario');
 const Estabelecimento = require('../model/Estabelecimento');
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
 
 module.exports = {
 
@@ -19,6 +22,14 @@ module.exports = {
     },
 
     async showbyday(req, res){
+        // 0 DOMINGO
+        // 1 SEGUNDA
+        // 2 TERCA
+        // 3 QUARTA
+        // 4 QUINTA
+        // 5 SEXTA
+        // 6 SABADO
+
         var jsonArr = [];
         var jsonEventos = [];
         const dia = new Date(req.params.data);
@@ -32,7 +43,7 @@ module.exports = {
             }
         }
 
-        if(diasemana == 6){
+        if(diasemana == 0){
             for (var i = returnSlots.hrinicio_domingo; i <= returnSlots.hrfim_domingo; i++) {
                 var status = jsonEventos.includes(i) ? 'I' : 'D';
                 jsonArr.push({
@@ -44,7 +55,7 @@ module.exports = {
             }
         }
 
-        if(diasemana >= 0 && diasemana < 5){
+        if(diasemana > 0 && diasemana < 6){
             for (var i = returnSlots.hrinicio_semana; i <= returnSlots.hrfim_semana; i++) {
                 var status = jsonEventos.includes(i) ? 'I' : 'D';
                 jsonArr.push({
@@ -56,7 +67,7 @@ module.exports = {
             }
         }
 
-        if(diasemana == 5){
+        if(diasemana == 6){
             for (var i = returnSlots.hrinicio_sabado; i <= returnSlots.hrfim_sabado; i++) {
                 var status = jsonEventos.includes(i) ? 'I' : 'D';
                 jsonArr.push({
@@ -81,7 +92,7 @@ module.exports = {
         return res.json(returnDel)
     },
 
-    store(req, res) {
+    async store(req, res) {
         const { data, hora, comentario, idestabelecimento, idusuario } = req.body;
 
         const returnPost = Evento.create({
@@ -90,6 +101,48 @@ module.exports = {
             comentario,
             idestabelecimento,
             idusuario
+        });
+
+        const user = await Usuario.findById({ _id: idusuario });
+        const estab = await Estabelecimento.findById({ _id: idestabelecimento });
+
+        let transporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "cb617fac631548",
+                pass: "e6242a10de30be"
+            }
+        });
+
+        const options = {
+            viewEngine: {
+              extName: ".handlebars",
+              partialsDir: './views/',
+              defaultLayout: false
+            },
+            viewPath: './views/',
+            extName: ".handlebars"
+          };
+
+        transporter.use('compile',hbs(options));
+
+        await transporter.sendMail({
+            from: '"EloyAqui" <noreply@eloyaqui.com.br>',
+            to: user.email,
+            subject: 'Novo Agendamento realizado com sucesso ✔',
+            text: 'Novo agendamento realizado com sucesso', 
+            template: 'agendamento',
+            context: {
+                nome : user.nome,
+                estabelecimento: estab.nome,
+                rua: estab.rua,
+                numero: estab.numero,
+                telefone: estab.telefone,
+                data: data,
+                hora: hora,
+                support_url:'mailto:suporte@eloyaqui.com.br'
+           }
         });
 
         return res.json(returnPost);
