1f687d47f7a178eecb4d752b7e73c00ed3de4c99
clz03 <cleber.znch@gmail.com>
2019-12-02 21:57:22 -0300

    alteracao controller precadastro

--- src/controllers/PrecadastroController.js
+++ src/controllers/PrecadastroController.js
@@ -23,11 +23,11 @@ module.exports = {
         return res.json(returnDel)
     },
 
-    async store(req, res) {
+    store(req, res) {
 
         const { nome, descr, rua, numero, bairro, cep, fone1, fone2, pedonline, email, facebook, instagram, whatsapp, hrinicio_semana, hrfim_semana, hrinicio_sabado, hrfim_sabado, hrinicio_domingo, hrfim_domingo,idcategoria } = req.body;
 
-        const returnPost = await Estabelecimento.create({
+        const returnPost = await Precadastro.create({
             nome, 
             descr, 
             rua, 
