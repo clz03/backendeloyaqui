cdc57c539d5b9ab5181256f052ea655de20da52f
clz03 <cleber.znch@gmail.com>
2020-01-27 16:04:43 -0300

    alteracao rota itempedido

--- src/controllers/ItensPedidoController.js
+++ src/controllers/ItensPedidoController.js
@@ -1,31 +1,31 @@
-const Pedido = require('../model/Pedido');
+const ItemPedido = require('../model/ItensPedido');
 
 module.exports = {
 
     async index(req, res){
-        const returnGet = await Pedido.find();
+        const returnGet = await ItemPedido.find();
         return res.json(returnGet)
     },
 
     async show(req, res){
-        const returnShow = await Pedido.find({ _id: req.params.id });
+        const returnShow = await ItemPedido.find({ _id: req.params.id });
         return res.json(returnShow)
     },
 
     async update(req, res){
-        const returnUpdate = await Pedido.updateOne({ _id: req.params.id },req.body);
+        const returnUpdate = await ItemPedido.updateOne({ _id: req.params.id },req.body);
         return res.json(returnUpdate)
     },
 
     async delete(req, res){
-        const returnDel = await Pedido.deleteOne({ _id: req.params.id });
+        const returnDel = await ItemPedido.deleteOne({ _id: req.params.id });
         return res.json(returnDel)
     },
 
     store(req, res) {
         const { item, valorun, valortotal, qtde, obs, idpedido } = req.body;
 
-        const returnPost = Pedido.create({
+        const returnPost = ItemPedido.create({
             item, 
             // descr, 
             valorun,
